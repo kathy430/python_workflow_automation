1ffdef4c6bac1ad89c709992893d264000e52325
Kathy Y. <itscathywithak@gmail.com>
2026-03-30 13:49:51 -0700

    Refactor forecast data processing in forecast_skus.py
    Refactored so the code will read directly from the source sheet and write the resulting data onto a Google Sheet.

--- forecast_skus.py
+++ forecast_skus.py
@@ -16,9 +16,7 @@ import numpy as np
 import os
 
 forecast_path = 'redacted'
-
-# open forecast path
-forecast = gc.open(forecast_path).sheet1
+forecast = gc.open(forecast_path).worksheet("redacted")
 
 # get data from google sheet
 data = forecast.get_all_values()
@@ -26,33 +24,36 @@ df = pd.DataFrame(data)
 
 display(df)
 
-# set df columns as first row
-df.columns = df.iloc[0]
-df = df.iloc[1:]
-display(df)
+# adjust rows and columns of dataframe for the data that matters
+fcst_df = df.iloc[1:, :-3]
+fcst_df.columns = fcst_df.iloc[0]
+fcst_df = fcst_df.iloc[1:].reset_index(drop=True)
 
-print(df['Brand'].unique())
+# remove EOL items from fcst_df
+status_to_exlude = ['EOL', 'EOL/Inactive']
+fcst_df = fcst_df[~fcst_df['STATUS'].isin(status_to_exlude)].copy()
+display(fcst_df)
 
-print(df.columns.tolist())
+print(fcst_df.columns.tolist())
 
 # only get the months from columns
-months = df.columns.tolist()[7:]
+months = fcst_df.columns.tolist()[6:]
 print(months)
 
 # change month columns to int
 for month in months:
   # Replace empty strings and 'N/A' with 0 before converting to int
-  df[month] = df[month].replace('', 0)
-  df[month] = df[month].replace('N/A', 0)
-  df[month] = df[month].replace('#REF!', 0)
-  df[month] = df[month].astype(int)
+  fcst_df[month] = fcst_df[month].replace('', 0)
+  fcst_df[month] = fcst_df[month].replace('N/A', 0)
+  fcst_df[month] = fcst_df[month].replace('#REF!', 0)
+  fcst_df[month] = fcst_df[month].astype(int)
 
 # formula to expand sets
 def expand_sets(row):
   barcode_field = str(row['redacted'])
   expanded = []
 
-  if (barcode_field == 'nan'):
+  if (barcode_field == 'nan' or barcode_field == ''):
     return pd.DataFrame()
 
   if '+' in barcode_field:
@@ -97,10 +98,30 @@ def expand_sets(row):
   return pd.DataFrame(expanded)
 
 # expand sets
-expanded_list = [expand_sets(row) for _, row in df.iterrows()]
+expanded_list = [expand_sets(row) for _, row in fcst_df.iterrows()]
 expanded_df = pd.concat(expanded_list, ignore_index=True)
 
 # group by sku
-grouped_df = expanded_df.groupby(['Brand', 'Barcode']).sum()
+grouped_df = expanded_df.groupby(['Brand', 'Barcode'], as_index=False).sum()
 
-grouped_df.to_csv('redacted')
+# add product description to grouped_df
+product_desc_df = fcst_df[['redacted', 'Description']]
+merged_df = grouped_df.merge(product_desc_df, left_on='Barcode', right_on='redacted', how='left').drop(columns=['redacted'])
+
+# move description after barcode
+col_to_move = merged_df.pop('Description')
+merged_df.insert(2, 'Description', col_to_move)
+merged_df['Description'] = merged_df['Description'].astype(str)
+merged_df
+
+# write data to google sheet
+result_sheet = gc.open('fcst in barcodes').sheet1
+
+# clear existing data
+result_sheet.clear()
+
+# convert dataframe to a list of lists
+data_to_write = [merged_df.columns.values.tolist()] + merged_df.values.tolist()
+print(data_to_write)
+# write data to google sheet
+result_sheet.update(data_to_write)
